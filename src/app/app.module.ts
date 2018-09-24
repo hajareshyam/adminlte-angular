0c178f71b70c157a5dfbc32d03906468bb7b0fe9
Shyam Hajare <hajareshyam@gmail.com>
2018-09-24 21:53:26 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AppmenuComponent } from './components/appmenu/appmenu.component';
+import { AppsettingComponent } from './components/appsetting/appsetting.component';
+import { AppfooterComponent } from './components/appfooter/appfooter.component';
+import { AppheaderComponent } from './components/appheader/appheader.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppmenuComponent,
+    AppsettingComponent,
+    AppfooterComponent,
+    AppheaderComponent
   ],
   imports: [
     BrowserModule
